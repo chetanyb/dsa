d56df641ff3150e884fdbf6d9e24b4328750b861
chetanyb <techbuddy022@gmail.com>
2024-02-28 13:26:27 +0530

    factorial iterative solution

--- Mathematics/Factorial/c++/factorial.cpp
+++ Mathematics/Factorial/c++/factorial.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 
-int factorial(int n);
+int factorialRecursive(int n);
+int factorialIterative(int n);
 
 int main()
 {
-    std::cout << "Factorial of 4 is " << factorial(4);
-    std::cout << "\nFactorial of 2 is " << factorial(2);
-    std::cout << "\nFactorial of 5 is " << factorial(5);
-    std::cout << "\nFactorial of 1 is " << factorial(1);
+    std::cout << "factorialRecursive of 4 is " << factorialRecursive(4);
+    std::cout << "\nfactorialRecursive of 2 is " << factorialRecursive(2);
+    std::cout << "\nfactorialRecursive of 5 is " << factorialRecursive(5);
+    std::cout << "\nfactorialRecursive of 1 is " << factorialRecursive(1);
+    std::cout << "\nfactorialIterative of 4 is " << factorialIterative(4);
+    std::cout << "\nfactorialIterative of 2 is " << factorialIterative(2);
+    std::cout << "\nfactorialIterative of 5 is " << factorialIterative(5);
+    std::cout << "\nfactorialIterative of 1 is " << factorialIterative(1) << "\n";
 }
 
-int factorial(int n)
+int factorialRecursive(int n)
 {
-    return (n == 0) ? 1 : (n * factorial(n - 1));
+    return (n == 0) ? 1 : (n * factorialRecursive(n - 1));
+}
+
+int factorialIterative(int n)
+{
+    int factorial = 1;
+    while (n > 0)
+    {
+        factorial *= n;
+        n -= 1;
+    }
+    return factorial;
 }
--- Mathematics/Factorial/rust/src/main.rs
+++ Mathematics/Factorial/rust/src/main.rs
@@ -1,17 +1,30 @@
 fn main() {
-    println!("Factorial of 4 is {}", factorial(4));
-    println!("Factorial of 2 is {}", factorial(2));
-    println!("Factorial of 5 is {}", factorial(5));
-    println!("Factorial of 1 is {}", factorial(1));
+    println!("factorial_recursive of 4 is {}", factorial_recursive(4));
+    println!("factorial_recursive of 2 is {}", factorial_recursive(2));
+    println!("factorial_recursive of 5 is {}", factorial_recursive(5));
+    println!("factorial_recursive of 1 is {}", factorial_recursive(1));
+    println!("factorial_iterative of 4 is {}", factorial_iterative(4));
+    println!("factorial_iterative of 2 is {}", factorial_iterative(2));
+    println!("factorial_iterative of 5 is {}", factorial_iterative(5));
+    println!("factorial_iterative of 1 is {}", factorial_iterative(1));
 }
 
-fn factorial(n: u32) -> u64 {
+fn factorial_recursive(n: u32) -> u64 {
     if n == 0 {
         1
     }
     else {
-        n as u64 * factorial(n - 1)
+        n as u64 * factorial_recursive(n - 1)
     }
+}
+
+fn factorial_iterative(mut n: u32) -> u64 {
+    let mut factorial: u64 = 1;
+    while n > 0 {
+        factorial *= n as u64;
+        n -= 1;
+    }
+    factorial
 }
 
 #[cfg(test)]
@@ -20,21 +33,25 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(factorial(4), 24);
+        assert_eq!(factorial_recursive(4), 24);
+        assert_eq!(factorial_iterative(4), 24);
     }
 
     #[test]
     fn test2() {
-        assert_eq!(factorial(1), 1);
+        assert_eq!(factorial_recursive(1), 1);
+        assert_eq!(factorial_iterative(1), 1);
     }
 
     #[test]
     fn test3() {
-        assert_eq!(factorial(0), 1);
+        assert_eq!(factorial_recursive(0), 1);
+        assert_eq!(factorial_iterative(0), 1);
     }
 
     #[test]
     fn test4() {
-        assert_eq!(factorial(12), 479001600);
+        assert_eq!(factorial_recursive(12), 479001600);
+        assert_eq!(factorial_iterative(12), 479001600);
     }
 }
